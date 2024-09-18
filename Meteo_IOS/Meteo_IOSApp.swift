ab77f382a35525bed266a437e5e6f72e3389a232
anthony <litv971@gmail.com>
2024-09-18 14:47:47 +0200

    Initial Commit

---
+++ Meteo_IOS/Meteo_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_IOSApp.swift
+//  Meteo_IOS
+//
+//  Created by Anthony Baucal on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
